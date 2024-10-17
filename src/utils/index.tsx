5e80d90d65883d34f622952ebdb598ed6ddb95fe
TanyaVerb <karra_t@mail.ru>
2024-10-17 16:54:05 +0300

    add comment

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -3,7 +3,7 @@
 export function classNames(mainClass, mods = {}, additional = []) {
   return [
     mainClass,
-    ...additional.filter(Boolean),
+    ...additional.filter(Boolean), //те классы, кот. не undefined или null
     ...Object.entries(mods)
       .filter(([className, value]) =>
         Boolean(className !== "undefined" && value)
